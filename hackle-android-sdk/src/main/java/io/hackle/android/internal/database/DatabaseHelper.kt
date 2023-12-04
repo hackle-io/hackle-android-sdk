538795dfcb2af66007eb6a2e0f913099ab2f3735
Wontak Kim <wontak@hackle.io>
2023-12-04 18:11:57 +0900

    [HACKLE-9106] 부분 누락 코드 수정

--- hackle-android-sdk/src/main/java/io/hackle/android/internal/database/DatabaseHelper.kt
+++ hackle-android-sdk/src/main/java/io/hackle/android/internal/database/DatabaseHelper.kt
@@ -1,16 +1,11 @@
 package io.hackle.android.internal.database
 
 import android.content.Context
-import io.hackle.android.internal.database.shared.SharedDatabase
 import io.hackle.android.internal.database.workspace.WorkspaceDatabase
 
 internal object DatabaseHelper {
 
     private val workspaceDatabases = hashMapOf<String, WorkspaceDatabase>()
-
-    @Synchronized
-        }
-    }
 
     @Synchronized
     fun getWorkspaceDatabase(context: Context, sdkKey: String): WorkspaceDatabase {
