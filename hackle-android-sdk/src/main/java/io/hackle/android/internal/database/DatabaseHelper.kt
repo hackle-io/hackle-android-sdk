8835de08e76b2c8f8d5b66b601485cd7cba7498d
Wontak Kim <wontak@hackle.io>
2023-12-07 21:42:12 +0900

    [HACKLE-9106] shared sqlite database 저장소 추가

--- hackle-android-sdk/src/main/java/io/hackle/android/internal/database/DatabaseHelper.kt
+++ hackle-android-sdk/src/main/java/io/hackle/android/internal/database/DatabaseHelper.kt
@@ -1,11 +1,21 @@
 package io.hackle.android.internal.database
 
 import android.content.Context
+import io.hackle.android.internal.database.shared.SharedDatabase
 import io.hackle.android.internal.database.workspace.WorkspaceDatabase
 
 internal object DatabaseHelper {
 
+    private var sharedDatabase: SharedDatabase? = null
     private val workspaceDatabases = hashMapOf<String, WorkspaceDatabase>()
+
+    @Synchronized
+    fun getSharedDatabase(context: Context): SharedDatabase {
+        if (sharedDatabase == null) {
+            sharedDatabase = SharedDatabase(context)
+        }
+        return sharedDatabase!!
+    }
 
     @Synchronized
     fun getWorkspaceDatabase(context: Context, sdkKey: String): WorkspaceDatabase {
---
+++ hackle-android-sdk/src/main/java/io/hackle/android/internal/database/shared/SharedDatabase.kt
@@ -0,0 +1,28 @@
+package io.hackle.android.internal.database.shared
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import io.hackle.android.internal.database.Database
+
+internal class SharedDatabase(
+    context: Context
+) : Database(
+    context = context,
+    name = DATABASE_NAME,
+    version = DATABASE_VERSION 
+) {
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(NotificationEntity.CREATE_TABLE)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        
+    }
+
+    companion object {
+
+        const val DATABASE_NAME = "hackle_shared"
+        const val DATABASE_VERSION = 1
+    }
+}
