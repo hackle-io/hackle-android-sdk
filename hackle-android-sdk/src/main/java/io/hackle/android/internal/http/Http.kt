ed0392e55dee558783eefff8c3fd7ef42fce7e17
yong <yong@hackle.io>
2024-08-20 17:20:06 +0900

    feat: UserExplorer override is applied to the server.

--- hackle-android-sdk/src/main/java/io/hackle/android/internal/http/Http.kt
+++ hackle-android-sdk/src/main/java/io/hackle/android/internal/http/Http.kt
@@ -1,6 +1,7 @@
 package io.hackle.android.internal.http
 
 import io.hackle.android.internal.utils.json.parseJson
+import okhttp3.MediaType
 import okhttp3.Response
 import okhttp3.ResponseBody
 import java.net.HttpURLConnection.HTTP_NOT_MODIFIED
@@ -15,3 +16,5 @@ internal inline fun <reified T> ResponseBody.parse(): T = string().parseJson()
 
 internal const val HEADER_LAST_MODIFIED = "Last-Modified"
 internal const val HEADER_IF_MODIFIED_SINCE = "If-Modified-Since"
+
+internal val CONTENT_TYPE_APPLICATION_JSON = MediaType.get("application/json; charset=utf-8")
--- hackle-android-sdk/src/main/java/io/hackle/android/ui/explorer/base/HackleUserExplorerService.kt
+++ hackle-android-sdk/src/main/java/io/hackle/android/ui/explorer/base/HackleUserExplorerService.kt
@@ -1,15 +1,21 @@
 package io.hackle.android.ui.explorer.base
 
+import io.hackle.android.internal.bridge.model.toDto
+import io.hackle.android.internal.devtools.DevToolsApi
+import io.hackle.android.internal.devtools.OverrideRequestDto
 import io.hackle.android.internal.push.token.PushTokenManager
+import io.hackle.android.internal.task.TaskExecutors.runOnBackground
 import io.hackle.android.internal.user.UserManager
 import io.hackle.android.ui.explorer.storage.HackleUserManualOverrideStorage
 import io.hackle.sdk.common.decision.Decision
 import io.hackle.sdk.common.decision.FeatureFlagDecision
 import io.hackle.sdk.core.HackleCore
+import io.hackle.sdk.core.internal.log.Logger
 import io.hackle.sdk.core.internal.metrics.Metrics
 import io.hackle.sdk.core.model.Experiment
 import io.hackle.sdk.core.model.Experiment.Type.AB_TEST
 import io.hackle.sdk.core.model.Experiment.Type.FEATURE_FLAG
+import io.hackle.sdk.core.model.Variation
 import io.hackle.sdk.core.user.HackleUser
 
 internal class HackleUserExplorerService(
@@ -17,7 +23,8 @@ internal class HackleUserExplorerService(
     private val userManager: UserManager,
     private val abTestOverrideStorage: HackleUserManualOverrideStorage,
     private val featureFlagOverrideStorage: HackleUserManualOverrideStorage,
-    private val pushTokenManager: PushTokenManager
+    private val pushTokenManager: PushTokenManager,
+    private val devToolsApi: DevToolsApi,
 ) {
 
     fun currentUser(): HackleUser {
@@ -36,19 +43,43 @@ internal class HackleUserExplorerService(
         return abTestOverrideStorage.getAll()
     }
 
-    fun setAbTestOverride(experiment: Experiment, variationId: Long) {
-        abTestOverrideStorage.set(experiment, variationId)
+    fun setAbTestOverride(experiment: Experiment, variation: Variation) {
+        abTestOverrideStorage.set(experiment, variation.id)
         increment(AB_TEST, "set")
+        runOnBackground {
+            try {
+                val requestDto = createOverrideRequest(variation)
+                devToolsApi.addExperimentOverrides(experiment.key, requestDto)
+            } catch (e: Exception) {
+                log.error { "Failed to add experiment overrides to server: $e" }
+            }
+        }
     }
 
-    fun resetAbTestOverride(experiment: Experiment) {
+    fun resetAbTestOverride(experiment: Experiment, variation: Variation) {
         abTestOverrideStorage.remove(experiment)
         increment(AB_TEST, "reset")
+        runOnBackground {
+            try {
+                val requestDto = createOverrideRequest(variation)
+                devToolsApi.removeExperimentOverrides(experiment.key, requestDto)
+            } catch (e: Exception) {
+                log.error { "Failed to remove experiment overrides to server: $e" }
+            }
+        }
     }
 
     fun resetAllAbTestOverride() {
         abTestOverrideStorage.clear()
         increment(AB_TEST, "reset.all")
+        runOnBackground {
+            try {
+                val requestDto = createOverrideRequest()
+                devToolsApi.removeAllExperimentOverrides(requestDto)
+            } catch (e: Exception) {
+                log.error { "Failed to remove all experiment overrides to server: $e" }
+            }
+        }
     }
 
     fun getFeatureFlagDecisions(): List<Pair<Experiment, FeatureFlagDecision>> {
@@ -59,19 +90,58 @@ internal class HackleUserExplorerService(
         return featureFlagOverrideStorage.getAll()
     }
 
-    fun setFeatureFlagOverride(experiment: Experiment, variationId: Long) {
-        featureFlagOverrideStorage.set(experiment, variationId)
+    fun setFeatureFlagOverride(experiment: Experiment, variation: Variation) {
+        featureFlagOverrideStorage.set(experiment, variation.id)
         increment(FEATURE_FLAG, "set")
+        runOnBackground {
+            try {
+                val requestDto = createOverrideRequest(variation)
+                devToolsApi.addFeatureFlagOverrides(experiment.key, requestDto)
+            } catch (e: Exception) {
+                log.error { "Failed to add feature flag overrides to server: $e" }
+            }
+        }
     }
 
-    fun resetFeatureFlagOverride(experiment: Experiment) {
+    fun resetFeatureFlagOverride(experiment: Experiment, variation: Variation) {
         featureFlagOverrideStorage.remove(experiment)
         increment(FEATURE_FLAG, "reset")
+        runOnBackground {
+            try {
+                val requestDto = createOverrideRequest(variation)
+                devToolsApi.removeFeatureFlagOverrides(experiment.key, requestDto)
+            } catch (e: Exception) {
+                log.error { "Failed to remove feature flag overrides to server: $e" }
+            }
+        }
     }
 
     fun resetAllFeatureFlagOverride() {
         featureFlagOverrideStorage.clear()
         increment(FEATURE_FLAG, "reset.all")
+        runOnBackground {
+            try {
+                val requestDto = createOverrideRequest()
+                devToolsApi.removeAllFeatureFlagOverrides(requestDto)
+            } catch (e: Exception) {
+                log.error { "Failed to remove all feature flag overrides to server: $e" }
+            }
+        }
+    }
+
+    private fun createOverrideRequest(variation: Variation? = null): OverrideRequestDto {
+        val hackleUser = currentUser()
+        val userDto = userManager.currentUser.toBuilder()
+            .id(hackleUser.id)
+            .deviceId(hackleUser.deviceId)
+            .userId(hackleUser.userId)
+            .properties(hackleUser.properties)
+            .build()
+            .toDto()
+        return OverrideRequestDto(
+            user = userDto,
+            variation = variation?.key
+        )
     }
 
     private fun increment(experimentType: Experiment.Type, operation: String) {
@@ -80,5 +150,9 @@ internal class HackleUserExplorerService(
             "operation" to operation
         )
         Metrics.counter("experiment.manual.override", tags).increment()
+    }
+
+    companion object {
+        private val log = Logger<HackleUserExplorerService>()
     }
 }
