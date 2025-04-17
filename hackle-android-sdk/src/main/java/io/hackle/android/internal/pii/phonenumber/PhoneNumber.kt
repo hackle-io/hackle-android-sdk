42f235121825512a3766fa7f155054b7dfb0b4e2
sungwoo-hackle <sungwoo.yeo@hackle.io>
2025-04-17 13:55:45 +0900

    remove validation

--- hackle-android-sdk/src/main/java/io/hackle/android/internal/pii/phonenumber/PhoneNumber.kt
+++ hackle-android-sdk/src/main/java/io/hackle/android/internal/pii/phonenumber/PhoneNumber.kt
@@ -3,10 +3,7 @@ package io.hackle.android.internal.pii.phonenumber
 internal data class PhoneNumber(val value: String) {
     companion object {
         fun create(phoneNumber: String): PhoneNumber {
-            val filteredValue = phoneNumber
-                .filter { it.isDigit() || it == '+' }
-                .take(16)
-            return PhoneNumber(filteredValue)
+            return PhoneNumber(phoneNumber)
         }
     }
 }
--- hackle-android-sdk/src/test/java/io/hackle/android/internal/pii/phonenumber/PhoneNumberTest.kt
+++ hackle-android-sdk/src/test/java/io/hackle/android/internal/pii/phonenumber/PhoneNumberTest.kt
@@ -5,52 +5,13 @@ import org.junit.Test
 
 class PhoneNumberTest {
     @Test
-    fun `filtered_phone_number_with_+`() {
+    fun `phone number`() {
         // Given
-        val phoneNumbers = arrayOf(
-            "+821012345678",
-            "+82-10-1234-5678",
-            "+82 10 1234 5678",
-            "+82 10 1234-5678",
-            "+82(10)12345678",
-            "+82(10)1234-5678",
-            "+82(10)1234 5678",
-            "+82-10-1234-5678aaa",
-            "aaa+82 10 1234 5678",
-        )
-        val expected = "+821012345678"
+        val phoneNumber = "+821012345678"
 
-        for (phoneNumber in phoneNumbers) {
-            // When
-            val result = PhoneNumber.create(phoneNumber)
+        val result = PhoneNumber.create(phoneNumber)
 
-            // Then
-            assertEquals(result.value, expected)
-        }
-    }
-
-    @Test
-    fun filtered_phone_number() {
-        // Given
-        val phoneNumbers = arrayOf(
-            "01012345678",
-            "01012345678",
-            "010-1234-5678",
-            "010 1234 5678",
-            "010 1234-5678",
-            "010(1234)5678",
-            "010(1234)5678",
-            "010(1234)5678aaa",
-            "aaa010 1234 5678",
-        )
-        val expected = "01012345678"
-
-        for (phoneNumber in phoneNumbers) {
-            // When
-            val result = PhoneNumber.create(phoneNumber)
-
-            // Then
-            assertEquals(result.value, expected)
-        }
+        // Then
+        assertEquals(result.value, phoneNumber)
     }
 }
