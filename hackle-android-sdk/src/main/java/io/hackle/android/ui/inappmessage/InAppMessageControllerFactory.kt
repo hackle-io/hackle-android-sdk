0e72554b232ee2d7f22a683d8a4d4b4e7f8a5961
sungwoo-hackle <sungwoo.yeo@hackle.io>
2025-04-24 18:30:13 +0900

    Modify it to use the insets provided in the listener instead of a separate function.

--- hackle-android-sdk/src/main/java/io/hackle/android/ui/inappmessage/InAppMessageControllerFactory.kt
+++ hackle-android-sdk/src/main/java/io/hackle/android/ui/inappmessage/InAppMessageControllerFactory.kt
@@ -2,10 +2,10 @@ package io.hackle.android.ui.inappmessage
 
 import android.annotation.SuppressLint
 import android.app.Activity
-import android.os.Build
+import android.view.View
 import android.view.ViewGroup
-import android.view.WindowInsets
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
 import io.hackle.android.internal.inappmessage.presentation.InAppMessagePresentationContext
 import io.hackle.android.ui.inappmessage.layout.view.InAppMessageViewController
@@ -36,64 +36,34 @@ internal class InAppMessageControllerFactory(
         val controller = InAppMessageViewController(view, context, ui)
         view.setController(controller)
 
-        // add margin when enableEdgeToEdge
-        // ref. https://developer.android.com/develop/ui/views/layout/edge-to-edge#system-bars-insets
         if(controller.context.message.layout.displayType == BANNER || controller.context.message.layout.displayType == BOTTOM_SHEET) {
-            ViewCompat.setOnApplyWindowInsetsListener(view) { v, windowInsets ->
-                val inAppMessageAlignment = controller.context.message.layout.alignment?.vertical
-
-                if(inAppMessageAlignment == InAppMessage.Message.Alignment.Vertical.TOP) {
-                    v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
-                        val statusBarHeight = getStatusBarHeight(activity)
-                        topMargin += statusBarHeight
-                    }
-                } else if(inAppMessageAlignment == InAppMessage.Message.Alignment.Vertical.BOTTOM) {
-                    v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
-                        val navigationBarHeight = getNavigationBarHeight(activity)
-                        bottomMargin += navigationBarHeight
-                    }
-                }
-                // 마진 적용 후 리스너 해제
-                ViewCompat.setOnApplyWindowInsetsListener(v, null)
-
-                windowInsets
-            }
+            setOnApplyWindowInsetsListener(view, controller)
         }
 
         view.configure()
         return controller
     }
 
-    @SuppressLint("InternalInsetResource")
-    private fun getNavigationBarHeight(activity: Activity): Int {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            val insets = activity.window.decorView.rootWindowInsets
-            return insets?.getInsets(WindowInsets.Type.navigationBars())?.bottom ?: 0
-        } else {
-            val resourceId =
-                activity.resources.getIdentifier("navigation_bar_height", "dimen", "android")
-            return if (resourceId > 0) {
-                activity.resources.getDimensionPixelSize(resourceId)
-            } else {
-                0
-            }
-        }
-    }
+    // add margin when enableEdgeToEdge
+    // ref. https://developer.android.com/develop/ui/views/layout/edge-to-edge#system-bars-insets
+    @SuppressLint("RestrictedApi")
+    private fun setOnApplyWindowInsetsListener(view: View, controller: InAppMessageController) {
+        ViewCompat.setOnApplyWindowInsetsListener(view) { v, windowInsets ->
+            val inAppMessageAlignment = controller.context.message.layout.alignment?.vertical
 
-    @SuppressLint("InternalInsetResource")
-    private fun getStatusBarHeight(activity: Activity): Int {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            val insets = activity.window.decorView.rootWindowInsets
-            insets?.getInsets(WindowInsets.Type.statusBars())?.top ?: 0
-        } else {
-            val resourceId = activity.resources.getIdentifier(
-                "status_bar_height", "dimen", "android"
-            )
-            if (resourceId > 0) {
-                activity.resources.getDimensionPixelSize(resourceId)
-            } else {
-                0
+            if(inAppMessageAlignment == InAppMessage.Message.Alignment.Vertical.TOP) {
+                v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                    topMargin += windowInsets.systemWindowInsetTop
+                }
+            } else if(inAppMessageAlignment == InAppMessage.Message.Alignment.Vertical.BOTTOM) {
+                v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                    bottomMargin += windowInsets.systemWindowInsetBottom
+                }
             }
+            // 마진 적용 후 리스너 해제
+            ViewCompat.setOnApplyWindowInsetsListener(v, null)
+
+            WindowInsetsCompat.CONSUMED
         }
     }
 }
