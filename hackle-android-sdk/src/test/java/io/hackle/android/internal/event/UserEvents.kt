1aea5ae470f96e3ec45c890d3d92af9d015f8d3f
yong <yong@hackle.io>
2025-05-19 11:23:43 +0900

    Added trigger event insertId to $in_app_impression event properties

--- hackle-android-sdk/src/test/java/io/hackle/android/internal/event/UserEvents.kt
+++ hackle-android-sdk/src/test/java/io/hackle/android/internal/event/UserEvents.kt
@@ -7,19 +7,22 @@ import io.hackle.sdk.core.user.HackleUser
 import io.hackle.sdk.core.user.IdentifierType
 import io.mockk.every
 import io.mockk.mockk
+import java.util.UUID
 
 object UserEvents {
 
     fun track(
         eventKey: String,
         user: HackleUser = HackleUser.builder().identifier(IdentifierType.ID, "user").build(),
-        timestamp: Long = System.currentTimeMillis()
+        timestamp: Long = System.currentTimeMillis(),
+        insertId: String = UUID.randomUUID().toString()
     ): UserEvent.Track {
         return mockk {
             every { this@mockk.eventType } returns EventType.Custom(1, eventKey)
             every { this@mockk.event } returns Event.builder(eventKey).build()
             every { this@mockk.user } returns user
             every { this@mockk.timestamp } returns timestamp
+            every { this@mockk.insertId } returns insertId
         }
     }
 }
