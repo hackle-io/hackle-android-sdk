1106547bbb40dbff7a507942b39e2e01ea3f52e4
yong <yong@hackle.io>
2025-07-08 15:31:48 +0900

    add tags to api metrics

--- hackle-android-sdk/src/main/java/io/hackle/android/internal/monitoring/metric/ApiCallMetrics.kt
+++ hackle-android-sdk/src/main/java/io/hackle/android/internal/monitoring/metric/ApiCallMetrics.kt
@@ -7,20 +7,24 @@ import okhttp3.Response
 
 internal object ApiCallMetrics {
 
+    private const val NONE_TAG = "NONE"
+
     inline fun record(operation: String, call: () -> Response): Response {
         val sample = Timer.start()
         return try {
-            call().also { record(operation, sample, it) }
-        } catch (e: Exception) {
-            record(operation, sample, null)
+            call().also { record(operation, sample, it, null) }
+        } catch (e: Throwable) {
+            record(operation, sample, null, e)
             throw e
         }
     }
 
-    fun record(operation: String, sample: Timer.Sample, response: Response?) {
+    fun record(operation: String, sample: Timer.Sample, response: Response?, e: Throwable?) {
         val tags = hashMapOf(
             "operation" to operation,
-            "success" to success(response)
+            "success" to success(response),
+            "status" to status(response),
+            "exception" to exception(e),
         )
         val timer = Metrics.timer("api.call", tags)
         sample.stop(timer)
@@ -32,5 +36,17 @@ internal object ApiCallMetrics {
         }
         val success = response.isSuccessful || response.isNotModified
         return success.toString()
+    }
+
+    private fun status(response: Response?): String {
+        return response?.code()?.toString() ?: NONE_TAG
+    }
+
+    private fun exception(e: Throwable?): String {
+        if (e == null) {
+            return NONE_TAG
+        }
+        val simpleName = e::class.java.simpleName
+        return simpleName.ifBlank { e::class.java.name }
     }
 }
