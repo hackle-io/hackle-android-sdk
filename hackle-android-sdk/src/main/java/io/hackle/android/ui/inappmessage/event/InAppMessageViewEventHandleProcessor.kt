28f7ad75fc72b46235957b8b63cbdcb5eeff516d
sungwoo-hackle <sungwoo.yeo@hackle.io>
2026-05-30 18:54:47 +0900

    chore: remove explanatory comments

--- hackle-android-sdk/src/main/java/io/hackle/android/ui/inappmessage/event/InAppMessageViewEventHandleProcessor.kt
+++ hackle-android-sdk/src/main/java/io/hackle/android/ui/inappmessage/event/InAppMessageViewEventHandleProcessor.kt
@@ -9,7 +9,6 @@ internal class InAppMessageViewEventHandleProcessor(
 
     fun process(view: InAppMessageView, event: InAppMessageViewEvent, types: List<InAppMessageViewEventHandleType>) {
         for (handleType in types) {
-            // 한 핸들러(트래킹/액션/호스트 콜백)의 예외가 다른 핸들러와 UI 클릭 콜백으로 전파되지 않도록 격리한다.
             try {
                 val handler = handlerFactory.get(handleType)
                 handler.handle(view, event)
