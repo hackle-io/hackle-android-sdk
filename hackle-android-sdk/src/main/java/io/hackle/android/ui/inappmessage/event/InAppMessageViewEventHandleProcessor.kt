f14b2e4a3a186efe422eefd0bf9e344bc873ae0c
sungwoo-hackle <sungwoo.yeo@hackle.io>
2026-06-01 16:55:14 +0900

    fix: isolate in-app message event handlers (#426)

--- hackle-android-sdk/src/main/java/io/hackle/android/ui/inappmessage/event/InAppMessageViewEventHandleProcessor.kt
+++ hackle-android-sdk/src/main/java/io/hackle/android/ui/inappmessage/event/InAppMessageViewEventHandleProcessor.kt
@@ -1,6 +1,7 @@
 package io.hackle.android.ui.inappmessage.event
 
 import io.hackle.android.ui.inappmessage.view.InAppMessageView
+import io.hackle.sdk.core.internal.log.Logger
 
 internal class InAppMessageViewEventHandleProcessor(
     private val handlerFactory: InAppMessageViewEventHandlerFactory,
@@ -8,8 +9,16 @@ internal class InAppMessageViewEventHandleProcessor(
 
     fun process(view: InAppMessageView, event: InAppMessageViewEvent, types: List<InAppMessageViewEventHandleType>) {
         for (handleType in types) {
-            val handler = handlerFactory.get(handleType)
-            handler.handle(view, event)
+            try {
+                val handler = handlerFactory.get(handleType)
+                handler.handle(view, event)
+            } catch (e: Throwable) {
+                log.error { "Failed to handle InAppMessage view event [$handleType]: $e" }
+            }
         }
+    }
+
+    companion object {
+        private val log = Logger<InAppMessageViewEventHandleProcessor>()
     }
 }
